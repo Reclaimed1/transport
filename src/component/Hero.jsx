1392ec75c7aef1918a207e920c840d3eccbfd636
Reclaimed1 <olanijunaid@gmail.com>
2025-07-29 14:14:06 +0100

    checking again

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,10 +2,12 @@ import Nav from '../component/Nav.jsx'
 import Afternav from '../component/Afternav.jsx'
 export default function Hero(){
     return(
- <div className='topImg h-[1035px] w-full bg-cover bg-center' >
-    <img src="" alt="" />
+ <div className='relative h-[1035px]' >
+    <img className='absolute h-[1035px] w-full bg-cover bg-center' src="background1.png" alt="" />
+    <div className=' absolute'>
     <Nav />
     <Afternav />
+    </div>
       </div>
       )
 }
