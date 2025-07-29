7991afe4703fe04e3a6771aa1db9d04b86cbf033
Reclaimed1 <olanijunaid@gmail.com>
2025-07-29 14:04:22 +0100

    hero edited

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,8 +1,10 @@
 import Nav from '../component/Nav.jsx'
 import Afternav from '../component/Afternav.jsx'
 export default function Hero(){
+    return(
  <div className='topImg h-[1035px] w-full bg-cover bg-center' >
     <Nav />
     <Afternav />
       </div>
+      )
 }
